8ddaccde5c6f0d069386459dfc92bea203683405
SeanMajic <57004221+SeanMajic@users.noreply.github.com>
2020-01-21 14:46:51 +0000

    Current Invasion On 5h Avenue Scripts

---
+++ Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float damage = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObjectThatPossesesACollider)
+    {
+        var health = otherObjectThatPossesesACollider.GetComponent<Health>();
+        var attacker = otherObjectThatPossesesACollider.GetComponent<Attacker>();
+
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+
+       
+
+    }
+
+}
